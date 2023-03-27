24719ad20689e3508ef859f416d9fa53377f3020
GO1T3R <eduardo_1999sosa@hotmail.com>
2023-03-26 22:46:59 -0500

    Programa funcionando

--- script.js
+++ script.js
@@ -13,29 +13,42 @@ function cargarEventListener() {
 
 function sacarPromedio(e) {
     e.preventDefault();
-
     let promedio = (parseFloat(nota1.value) + parseFloat(nota2.value) + parseFloat(nota3.value))/3;
-    
     let obs;
 
-    if(promedio > 6 && promedio <= 10) {
-        obs =value= "!Aprobado¡ &#x2714";
-    } else {
-        obs =value= "!Desaprobado¡ &#x274c";
+    if(nombre.value === ""){
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            footer: '<a href="">Why do I have this issue?</a>'
+          })
+    }if(nota1 === ""||nota2=== ""|| nota3 === "") {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Te falto llenar una nota!'
+          })
     }
-//    console.log(obs)
-    agregartabla();
-}
 
-function agregartabla() {
+    if(promedio > 6 && promedio <= 10) {
+        obs =value= "Aprobado &#x2714"; 
+    }else {
+        obs =value= "Desaprobado &#x274c";
+    }
+
+
+//    console.log(obs)
+    agregartabla(promedio, obs);
+ }
+
+function agregartabla(promedio, obs) {
     tabla.innerHTML = `
     <td>${nombre.value}</td>
     <td>${nota1.value}</td>
     <td>${nota2.value}</td>
     <td>${nota3.value}</td>
+    <td>${promedio.toFixed(1)}</td>
+    <td>${obs}</td>
     `;
-
-    
-    
-
 }
