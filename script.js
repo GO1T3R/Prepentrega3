8b32a7cc31b7332643274c7655c430a35f859b81
GO1T3R <eduardo_1999sosa@hotmail.com>
2023-03-27 10:39:46 -0500

    Falta corrigir el duplicado del arrayObject

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let nota3 = document.getElementById('nota3');
 const boton = document.querySelector('.btn-send');
 const tabla = document.getElementById('addtabla');
 
+let infoAlumno = [];
+
+
 cargarEventListener();
 // Cargar EventListener
 function cargarEventListener() {
@@ -16,12 +19,12 @@ function sacarPromedio(e) {
     let promedio = (parseFloat(nota1.value) + parseFloat(nota2.value) + parseFloat(nota3.value))/3;
     let obs;
 
+    // Condicion si se rellena los inputs
     if(nombre.value === ""){
         Swal.fire({
             icon: 'error',
             title: 'Oops...',
-            text: 'Something went wrong!',
-            footer: '<a href="">Why do I have this issue?</a>'
+            text: 'Te falto agregar el nombre'
           })
     }if(nota1 === ""||nota2=== ""|| nota3 === "") {
         Swal.fire({
@@ -31,24 +34,59 @@ function sacarPromedio(e) {
           })
     }
 
-    if(promedio > 6 && promedio <= 10) {
+    // condicion si el alumno pasa o no
+    if(promedio >= 7 && promedio <= 10) {
         obs =value= "Aprobado &#x2714"; 
     }else {
         obs =value= "Desaprobado &#x274c";
     }
-
-
-//    console.log(obs)
+    leerNotas();
     agregartabla(promedio, obs);
- }
-
-function agregartabla(promedio, obs) {
-    tabla.innerHTML = `
-    <td>${nombre.value}</td>
-    <td>${nota1.value}</td>
-    <td>${nota2.value}</td>
-    <td>${nota3.value}</td>
-    <td>${promedio.toFixed(1)}</td>
-    <td>${obs}</td>
-    `;
 }
+
+function leerNotas() {
+     
+    let infoNotas = {
+        Alumno: nombre.value,
+        CalificacionUno: nota1.value,
+        CalificacionDos: nota2.value,
+        CalificacionTres: nota2.value,
+    }
+
+    infoAlumno = [...infoAlumno, infoNotas];
+
+    console.log(infoAlumno);
+
+   
+}
+
+// Agrega los datos ingresado en tabla HTML
+function agregartabla(promedio, obs) {
+    limpiarHTML();
+
+    infoAlumno.forEach(notas => {
+        const row = document.createElement('tr')
+        row.innerHTML = `
+        <td>${nombre.value}</td>
+        <td>${nota1.value}</td>
+        <td>${nota2.value}</td>
+        <td>${nota3.value}</td>
+        <td>${promedio.toFixed(1)}</td>
+        <td>${obs}</td>
+        `;
+
+        // Agregar al html a tbody
+        tabla.appendChild(row);
+    })
+
+
+}
+
+
+//Limpiar el HTML para evitar que se repitan los objectos 
+function limpiarHTML() {
+    while(tabla.firstChild) {
+        tabla.removeChild(tabla.firstChild);
+    }
+}
+
